d011781bfbf347bc0cbe69045d60df03b6bf8ea1
Radoslav Kirilov <rkirilow@gmail.com>
2018-06-29 02:44:46 +0300

    feat(auth-token): default implementation of `TokenPayload.isValid()`

--- packages/ngx-api-utils/src/lib/auth-token/token-payload/token-payload.ts
+++ packages/ngx-api-utils/src/lib/auth-token/token-payload/token-payload.ts
@@ -2,6 +2,8 @@ export abstract class TokenPayload {
   /**
    * Shows if the token is valid, e.g. it has not expired
    */
-  abstract isValid(): boolean;
+  isValid() {
+    return this.expires ? Date.now() < this.expires().valueOf() : true;
+  }
   abstract expires?(): Date;
 }
