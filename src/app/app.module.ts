37c4cd29341b110f2b11c0fbfdec5f97d2133fb3
Radoslav Kirilov <rkirilow@gmail.com>
2018-07-26 10:24:22 +0300

    feat(ngx-api-utils): `NgxApiUtils#forRoot` implemented again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxApiUtilsModule } from 'ngx-api-utils';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
@@ -12,10 +13,16 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgxApiUtilsModule.forRoot({
+      baseUrl: '//localhost:3000',
+      authTokenAutoRemove: true,
+      authGuardPublicOnlyRoutes: /^\/(customer\/auth)([\/#?].*)?$/,
+      authGuardUrlForAuthenticated: '/customer/',
+      authGuardUrlForAuthentication: '/customer/auth/sign-in'
+    }),
     CoreModule.forRoot()
   ],
-  providers: [
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
