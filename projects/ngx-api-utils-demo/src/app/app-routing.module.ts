85f98e4932364e215b2d306c0434292b8e69f3bc
Radoslav Kirilov <rkirilow@gmail.com>
2022-01-30 12:06:34 +0200

    chore: update demo to Angular 11

--- projects/ngx-api-utils-demo/src/app/app-routing.module.ts
+++ projects/ngx-api-utils-demo/src/app/app-routing.module.ts
@@ -33,7 +33,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {enableTracing: false})],
+  imports: [RouterModule.forRoot(routes, {enableTracing: false, relativeLinkResolution: 'legacy'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
